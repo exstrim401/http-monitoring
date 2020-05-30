4943491248cdf8ea1f45aa927a2e142f5c743a01
exstrim401 <exstrim401@tuta.io>
2020-05-30 20:16:54 +0500

    Вынес конфигурацию в отдельный файл

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
+import json
 from email.message import EmailMessage
 import asyncio
 import aiosmtplib
 import aiohttp
 
-MAIL_HOST = ""
-MAIL_PORT = 25
-MAIL_HAS_TLS = False
-MAIL_TO = ""
-MAIL_FROM = ""
-MAIL_USER = None
-MAIL_PASSWORD = None
-URLS = ["https://google.com"]
+config = None
 
 
 async def main():
+    global config
     session = aiohttp.ClientSession()
     while True:
-        for url in URLS:
-            async with session.get(url) as resp:
-                print(f"{url} - {resp.status}")
-                if resp.status != 200:
-                    await report_url(url, resp.status)
+        with open("config.json") as f:
+            config = json.loads(f.read())
+        for url in config["urls"]:
+            try:
+                async with session.get(url) as resp:
+                    print(f"{url} - {resp.status}")
+                    if resp.status != 200:
+                        await report_url(url, resp.status)
+            except Exception as e:
+                print(f"{url} - {str(e)}")
+                await report_url(url, str(e))
         await asyncio.sleep(60*10)
 
 
 async def report_url(url, resp):
     message = EmailMessage()
-    message["From"] = MAIL_FROM
-    message["To"] = MAIL_TO
-    message["Subject"] = "Ошибка сервера"
-    message.set_content(f"Сервер по адресу {url} выдал {resp}")
-    await aiosmtplib.send(message, hostname=MAIL_HOST, port=MAIL_PORT,
-                          username=MAIL_USER, password=MAIL_PASSWORD,
-                          use_tls=MAIL_HAS_TLS)
+    message["From"] = config["mail_user"]
+    message["Subject"] = config["mail_subject"]
+    message.set_content(config["mail_content"].format(url, resp))
+    for email in config["mail_to"]:
+        message["To"] = email
+        await aiosmtplib.send(message, hostname=config["mail_host"],
+                              port=config["mail_port"],
+                              use_tls=config["mail_tls"])
 
 
 asyncio.run(main())
